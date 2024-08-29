8a57f1ce05b24c5c354d71e027ba2e9764404385
T3Marius <168598779+T3Marius@users.noreply.github.com>
2024-08-29 03:53:22 +0300

    Add files via upload

---
+++ Commands/Commands.cs
@@ -0,0 +1,152 @@
+﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Admin;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Utils;
+using CounterStrikeSharp.API;
+using static ChangeTeams.Config_Config;
+using static ChangeTeams.ChangeTeams;
+
+namespace ChangeTeams
+{
+    public static class Commands
+    {
+        public static void Load()
+        {
+            // Load the configuration
+            Config_Config.Load();
+
+            // Get configurations for admin and player commands
+            var adminConfig = Config_Config.Config.CommandsAdmin;
+            var playerConfig = Config_Config.Config.CommandsPlayer;
+
+            // Register commands based on configuration
+            var commands = new Dictionary<IEnumerable<string>, (string description, CommandInfo.CommandCallback handler)>
+            {
+                { adminConfig.MoveTerrorist, ("AdminTcommand", Command_MoveTerrorist) },
+                { adminConfig.MoveCounterTerrorist, ("AdminCTCommand", Command_MoveCounterTerrorist) },
+                { adminConfig.MoveSpectate, ("AdminSpecCommand", Command_MoveSpectate) },
+                { adminConfig.Swapper, ("AdminSwapCommand", Command_Swap) },
+                { playerConfig.Terrorist, ("PlayerTcommand", Command_Terrorist) },
+                { playerConfig.CounterTerrorist, ("PlayerCTcommand", Command_CounterTerrorist) },
+                { playerConfig.Spectate, ("PlayerSpecCommand", Command_Afk) }
+            };
+
+            // Add commands to the instance
+            foreach (var commandPair in commands)
+            {
+                foreach (var command in commandPair.Key)
+                {
+                    Instance.AddCommand($"css_{command}", commandPair.Value.description, commandPair.Value.handler);
+                }
+            }
+        }
+
+        [CommandHelper(minArgs: 0, whoCanExecute: CommandUsage.CLIENT_ONLY)]
+        public static void Command_Terrorist(CCSPlayerController? player, CommandInfo command)
+        {
+            if (player == null) return;
+
+            player.ChangeTeam(CsTeam.Terrorist);
+            player.PrintToChat(Config.Tag + Instance.Localizer["player.terrorist"]);
+        }
+
+        [CommandHelper(minArgs: 0, whoCanExecute: CommandUsage.CLIENT_ONLY)]
+        public static void Command_CounterTerrorist(CCSPlayerController? player, CommandInfo command)
+        {
+            if (player == null) return;
+
+            player.ChangeTeam(CsTeam.CounterTerrorist);
+            player.PrintToChat(Config.Tag + Instance.Localizer["player.counter_terrorist"]);
+        }
+
+        [CommandHelper(minArgs: 0, whoCanExecute: CommandUsage.CLIENT_ONLY)]
+        public static void Command_Afk(CCSPlayerController? player, CommandInfo command)
+        {
+            if (player == null) return;
+
+            player.ChangeTeam(CsTeam.Spectator);
+            player.PrintToChat(Config.Tag + Instance.Localizer["player.afk"]);
+        }
+
+        [CommandHelper(minArgs: 1, "<name or #userid>", whoCanExecute: CommandUsage.CLIENT_AND_SERVER)]
+        public static void Command_MoveTerrorist(CCSPlayerController? player, CommandInfo command)
+        {
+            if (player == null || !HasPermission(player)) return;
+
+            var target = FindPlayer(command.ArgByIndex(1));
+            if (target != null)
+            {
+                target.ChangeTeam(CsTeam.Terrorist);
+                target.PrintToChat(string.Format(Config.Tag + Instance.Localizer["admin.terrorist"], player.PlayerName));
+                Server.PrintToChatAll(string.Format(Config.Tag + Instance.Localizer["player.tmove.announcement"], target.PlayerName, player.PlayerName));
+            }
+        }
+
+        [CommandHelper(minArgs: 1, "<name or #userid>", whoCanExecute: CommandUsage.CLIENT_AND_SERVER)]
+        public static void Command_MoveCounterTerrorist(CCSPlayerController? player, CommandInfo command)
+        {
+            if (player == null || !HasPermission(player)) return;
+
+            var target = FindPlayer(command.ArgByIndex(1));
+            if (target != null)
+            {
+                target.ChangeTeam(CsTeam.CounterTerrorist);
+                target.PrintToChat(string.Format(Config.Tag + Instance.Localizer["admin.counter_terrorist"], player.PlayerName));
+                Server.PrintToChatAll(string.Format(Config.Tag + Instance.Localizer["player.ctmove.announcement"], target.PlayerName, player.PlayerName));
+            }
+        }
+
+        [CommandHelper(minArgs: 1, "<name or #userid>", whoCanExecute: CommandUsage.CLIENT_AND_SERVER)]
+        public static void Command_MoveSpectate(CCSPlayerController? player, CommandInfo command)
+        {
+            if (player == null || !HasPermission(player)) return;
+
+            var target = FindPlayer(command.ArgByIndex(1));
+            if (target != null)
+            {
+                target.ChangeTeam(CsTeam.Spectator);
+                target.PrintToChat(string.Format(Config.Tag + Instance.Localizer["admin.spectator"], player.PlayerName));
+                Server.PrintToChatAll(string.Format(Config.Tag + Instance.Localizer["player.spec_move.announcement"], target.PlayerName, player.PlayerName));
+            }
+        }
+
+        [CommandHelper(minArgs: 1, "<name or #userid>", whoCanExecute: CommandUsage.CLIENT_AND_SERVER)]
+        public static void Command_Swap(CCSPlayerController? player, CommandInfo command)
+        {
+            if (player == null || !HasPermission(player)) return;
+
+            var target = FindPlayer(command.ArgByIndex(1));
+            if (target != null)
+            {
+                Swap(player, target);
+            }
+        }
+
+        // Helper Methods
+        private static bool HasPermission(CCSPlayerController player)
+        {
+            return Config.Permissions.Count == 0 || Config.Permissions.Any(permission => AdminManager.PlayerHasPermissions(player, permission));
+        }
+
+        private static CCSPlayerController? FindPlayer(string identifier)
+        {
+            return Utilities.GetPlayers().FirstOrDefault(p => p.PlayerName == identifier || p.SteamID.ToString() == identifier);
+        }
+
+        private static void Swap(CCSPlayerController player, CCSPlayerController target)
+        {
+            if (target.Team == CsTeam.Terrorist)
+            {
+                target.ChangeTeam(CsTeam.CounterTerrorist);
+                target.PrintToChat(string.Format(Config.Tag + Instance.Localizer["admin.counter_terrorist"], player.PlayerName));
+                Server.PrintToChatAll(string.Format(Config.Tag + Instance.Localizer["player.ctmove.announcement"], target.PlayerName, player.PlayerName));
+            }
+            else if (target.Team == CsTeam.CounterTerrorist)
+            {
+                target.ChangeTeam(CsTeam.Terrorist);
+                target.PrintToChat(string.Format(Config.Tag + Instance.Localizer["admin.terrorist"], player.PlayerName));
+                Server.PrintToChatAll(string.Format(Config.Tag + Instance.Localizer["player.tmove.announcement"], target.PlayerName, player.PlayerName));
+            }
+        }
+    }
+}
--- Config/config.cs
+++ Config/config.cs
@@ -1,34 +1,99 @@
-﻿using CounterStrikeSharp.API.Core;
-using CounterStrikeSharp.API.Modules.Utils;
-using System.Text.Json.Serialization;
-public class PluginConfig : BasePluginConfig
+﻿using CounterStrikeSharp.API.Core.Translations;
+using CounterStrikeSharp.API;
+using System.Reflection;
+using Tomlyn.Model;
+using Tomlyn;
+
+namespace ChangeTeams
 {
-    [JsonPropertyName("Tag")] public string Tag { get; set; } = "{red}[SWAP]{default} ";
-
-    [JsonPropertyName("Permissions")] public List<string> Permissions { get; set; } = new List<string>();
-    public Config_Commands_Player CommandsPlayer { get; set; } = new Config_Commands_Player();
-    public Config_Commands_Admin CommandsAdmin { get; set; } = new Config_Commands_Admin();
-
-    public class Config_Commands_Player
+    public static class Config_Config
     {
-        [JsonPropertyName("Terrorist")]
-        public string[] Terrorist { get; set; } = { "t", "terrorist" };
+        public static Cfg Config { get; set; } = new Cfg();
 
-        [JsonPropertyName("Spectator")]
-        public string[] Spectate { get; set; } = { "afk", "spectate" };
-    }
-    public class Config_Commands_Admin
-    {
-        [JsonPropertyName("Terrorist")]
-        public string[] Terrorist { get; set; } = { "movet", "moveterrorist" };
+        public static void Load()
+        {
+            string assemblyName = Assembly.GetExecutingAssembly().GetName().Name ?? "";
+            string cfgPath = $"{Server.GameDirectory}/csgo/addons/counterstrikesharp/configs/plugins/{assemblyName}";
 
-        [JsonPropertyName("CounterTerrorist")]
-        public string[] CounterTerrorist { get; set; } = { "movect", "movecounterterrorist" };
+            LoadConfig($"{cfgPath}/config.toml");
+        }
 
-        [JsonPropertyName("Spectator")]
-        public string[] Spectate { get; set; } = { "spec", "movespectate" };
+        private static void LoadConfig(string configPath)
+        {
+            if (!File.Exists(configPath))
+            {
+                throw new FileNotFoundException($"Configuration file not found: {configPath}");
+            }
 
-        [JsonPropertyName("Swapper")]
-        public string[] Swapper { get; set; } = { "swap" };
+            string configText = File.ReadAllText(configPath);
+            TomlTable model = Toml.ToModel(configText);
+
+            TomlTable tagTable = (TomlTable)model["Tag"];
+            string config_tag = StringExtensions.ReplaceColorTags(tagTable["Tag"].ToString()!);
+
+            TomlTable permissionsTable = (TomlTable)model["Permissions"];
+            List<string> permissionsList = new();
+            foreach (var permission in (TomlArray)permissionsTable["FLAGS"])
+            {
+                permissionsList.Add(permission!.ToString()!);
+            }
+
+            TomlTable commandsTable = (TomlTable)model["CommandsPlayer"];
+            Config_Commands_Player config_playercommands = new()
+            {
+                Terrorist = GetTomlArray(commandsTable, "Terrorist"),
+                CounterTerrorist = GetTomlArray(commandsTable, "CounterTerrorist"),
+                Spectate = GetTomlArray(commandsTable, "Spectate")
+            };
+
+            TomlTable admincommandsTable = (TomlTable)model["CommandsAdmin"];
+            Config_Commands_Admin config_admincommands = new()
+            {
+                MoveTerrorist = GetTomlArray(admincommandsTable, "MoveTerrorist"),
+                MoveCounterTerrorist = GetTomlArray(admincommandsTable, "MoveCounterTerrorist"),
+                MoveSpectate = GetTomlArray(admincommandsTable, "MoveSpectate"),
+                Swapper = GetTomlArray(admincommandsTable, "Swapper")
+            };
+
+            Config = new Cfg
+            {
+                Tag = config_tag,
+                Permissions = permissionsList,
+                CommandsAdmin = config_admincommands,
+                CommandsPlayer = config_playercommands
+            };
+        }
+
+        private static string[] GetTomlArray(TomlTable table, string key)
+        {
+            if (table.TryGetValue(key, out var value) && value is TomlArray array)
+            {
+                return array.OfType<string>().ToArray();
+            }
+            return Array.Empty<string>();
+        }
+
+        public class Cfg
+        {
+            public string Tag { get; set; } = string.Empty;
+            public List<string> Permissions { get; set; } = new();
+            public Config_Commands_Player CommandsPlayer { get; set; } = new();
+            public Config_Commands_Admin CommandsAdmin { get; set; } = new();
+        }
+
+        public class Config_Commands_Player
+        {
+            public string[] Terrorist { get; set; } = Array.Empty<string>();
+            public string[] Spectate { get; set; } = Array.Empty<string>();
+            public string[] CounterTerrorist { get; set; } = Array.Empty<string>();
+        }
+
+        public class Config_Commands_Admin
+        {
+            public string[] MoveTerrorist { get; set; } = Array.Empty<string>();
+            public string[] MoveCounterTerrorist { get; set; } = Array.Empty<string>();
+            public string[] MoveSpectate { get; set; } = Array.Empty<string>();
+            public string[] Swapper { get; set; } = Array.Empty<string>();
+        }
     }
 }
