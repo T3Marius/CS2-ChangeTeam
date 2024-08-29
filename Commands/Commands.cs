59cfe9033ae04742ff7edb048efa79232465bea0
T3Marius <168598779+T3Marius@users.noreply.github.com>
2024-08-29 22:55:05 +0300

    Update Commands.cs

--- Commands/Commands.cs
+++ Commands/Commands.cs
@@ -1,4 +1,4 @@
-﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Admin;
 using CounterStrikeSharp.API.Modules.Commands;
 using CounterStrikeSharp.API.Modules.Utils;
@@ -12,14 +12,11 @@ namespace ChangeTeams
     {
         public static void Load()
         {
-            // Load the configuration
             Config_Config.Load();
 
-            // Get configurations for admin and player commands
             var adminConfig = Config_Config.Config.CommandsAdmin;
             var playerConfig = Config_Config.Config.CommandsPlayer;
 
-            // Register commands based on configuration
             var commands = new Dictionary<IEnumerable<string>, (string description, CommandInfo.CommandCallback handler)>
             {
                 { adminConfig.MoveTerrorist, ("AdminTcommand", Command_MoveTerrorist) },
@@ -27,11 +24,9 @@ namespace ChangeTeams
                 { adminConfig.MoveSpectate, ("AdminSpecCommand", Command_MoveSpectate) },
                 { adminConfig.Swapper, ("AdminSwapCommand", Command_Swap) },
                 { playerConfig.Terrorist, ("PlayerTcommand", Command_Terrorist) },
-                { playerConfig.CounterTerrorist, ("PlayerCTcommand", Command_CounterTerrorist) },
                 { playerConfig.Spectate, ("PlayerSpecCommand", Command_Afk) }
             };
 
-            // Add commands to the instance
             foreach (var commandPair in commands)
             {
                 foreach (var command in commandPair.Key)
@@ -51,15 +46,6 @@ namespace ChangeTeams
         }
 
         [CommandHelper(minArgs: 0, whoCanExecute: CommandUsage.CLIENT_ONLY)]
-        public static void Command_CounterTerrorist(CCSPlayerController? player, CommandInfo command)
-        {
-            if (player == null) return;
-
-            player.ChangeTeam(CsTeam.CounterTerrorist);
-            player.PrintToChat(Config.Tag + Instance.Localizer["player.counter_terrorist"]);
-        }
-
-        [CommandHelper(minArgs: 0, whoCanExecute: CommandUsage.CLIENT_ONLY)]
         public static void Command_Afk(CCSPlayerController? player, CommandInfo command)
         {
             if (player == null) return;
@@ -69,68 +55,96 @@ namespace ChangeTeams
         }
 
         [CommandHelper(minArgs: 1, "<name or #userid>", whoCanExecute: CommandUsage.CLIENT_AND_SERVER)]
-        public static void Command_MoveTerrorist(CCSPlayerController? player, CommandInfo command)
+        public static void Command_MoveTerrorist(CCSPlayerController? caller, CommandInfo command)
         {
-            if (player == null || !HasPermission(player)) return;
+            if (caller == null || !HasPermission(caller)) return;
 
-            var target = FindPlayer(command.ArgByIndex(1));
-            if (target != null)
+            var target = FindPlayer(command.ArgByIndex(1), caller);
+            if (target == null || !target.IsValid || target.Connected != PlayerConnectedState.PlayerConnected)
             {
-                target.ChangeTeam(CsTeam.Terrorist);
-                target.PrintToChat(string.Format(Config.Tag + Instance.Localizer["admin.terrorist"], player.PlayerName));
-                Server.PrintToChatAll(string.Format(Config.Tag + Instance.Localizer["player.tmove.announcement"], target.PlayerName, player.PlayerName));
+                return;
             }
+
+            target.ChangeTeam(CsTeam.Terrorist);
+            target.PrintToChat(string.Format(Config.Tag + Instance.Localizer["admin.terrorist"], caller.PlayerName));
+            Server.PrintToChatAll(string.Format(Config.Tag + Instance.Localizer["player.tmove.announcement"], target.PlayerName, caller.PlayerName));
         }
 
         [CommandHelper(minArgs: 1, "<name or #userid>", whoCanExecute: CommandUsage.CLIENT_AND_SERVER)]
-        public static void Command_MoveCounterTerrorist(CCSPlayerController? player, CommandInfo command)
+        public static void Command_MoveCounterTerrorist(CCSPlayerController? caller, CommandInfo command)
         {
-            if (player == null || !HasPermission(player)) return;
+            if (caller == null || !HasPermission(caller)) return;
 
-            var target = FindPlayer(command.ArgByIndex(1));
-            if (target != null)
+            var target = FindPlayer(command.ArgByIndex(1), caller);
+            if (target == null || !target.IsValid || target.Connected != PlayerConnectedState.PlayerConnected)
             {
-                target.ChangeTeam(CsTeam.CounterTerrorist);
-                target.PrintToChat(string.Format(Config.Tag + Instance.Localizer["admin.counter_terrorist"], player.PlayerName));
-                Server.PrintToChatAll(string.Format(Config.Tag + Instance.Localizer["player.ctmove.announcement"], target.PlayerName, player.PlayerName));
+                return;
             }
+
+            target.ChangeTeam(CsTeam.CounterTerrorist);
+            target.PrintToChat(string.Format(Config.Tag + Instance.Localizer["admin.counter_terrorist"], caller.PlayerName));
+            Server.PrintToChatAll(string.Format(Config.Tag + Instance.Localizer["player.ctmove.announcement"], target.PlayerName, caller.PlayerName));
         }
 
         [CommandHelper(minArgs: 1, "<name or #userid>", whoCanExecute: CommandUsage.CLIENT_AND_SERVER)]
-        public static void Command_MoveSpectate(CCSPlayerController? player, CommandInfo command)
+        public static void Command_MoveSpectate(CCSPlayerController? caller, CommandInfo command)
         {
-            if (player == null || !HasPermission(player)) return;
+            if (caller == null || !HasPermission(caller)) return;
 
-            var target = FindPlayer(command.ArgByIndex(1));
-            if (target != null)
+            var target = FindPlayer(command.ArgByIndex(1), caller);
+            if (target == null || !target.IsValid || target.Connected != PlayerConnectedState.PlayerConnected)
             {
-                target.ChangeTeam(CsTeam.Spectator);
-                target.PrintToChat(string.Format(Config.Tag + Instance.Localizer["admin.spectator"], player.PlayerName));
-                Server.PrintToChatAll(string.Format(Config.Tag + Instance.Localizer["player.spec_move.announcement"], target.PlayerName, player.PlayerName));
+                return;
             }
+
+            target.ChangeTeam(CsTeam.Spectator);
+            target.PrintToChat(string.Format(Config.Tag + Instance.Localizer["admin.spectator"], caller.PlayerName));
+            Server.PrintToChatAll(string.Format(Config.Tag + Instance.Localizer["player.spec_move.announcement"], target.PlayerName, caller.PlayerName));
         }
 
         [CommandHelper(minArgs: 1, "<name or #userid>", whoCanExecute: CommandUsage.CLIENT_AND_SERVER)]
-        public static void Command_Swap(CCSPlayerController? player, CommandInfo command)
+        public static void Command_Swap(CCSPlayerController? caller, CommandInfo command)
         {
-            if (player == null || !HasPermission(player)) return;
+            if (caller == null || !HasPermission(caller)) return;
 
-            var target = FindPlayer(command.ArgByIndex(1));
-            if (target != null)
+            var target = FindPlayer(command.ArgByIndex(1), caller);
+            if (target == null || !target.IsValid || target.Connected != PlayerConnectedState.PlayerConnected)
             {
-                Swap(player, target);
+                return;
             }
+
+            Swap(caller, target);
         }
 
-        // Helper Methods
         private static bool HasPermission(CCSPlayerController player)
         {
             return Config.Permissions.Count == 0 || Config.Permissions.Any(permission => AdminManager.PlayerHasPermissions(player, permission));
         }
 
-        private static CCSPlayerController? FindPlayer(string identifier)
+        private static CCSPlayerController? FindPlayer(string identifier, CCSPlayerController? caller)
         {
-            return Utilities.GetPlayers().FirstOrDefault(p => p.PlayerName == identifier || p.SteamID.ToString() == identifier);
+            if (identifier == "@me" && caller != null)
+            {
+                return caller;
+            }
+            var matchingPlayers = Utilities.GetPlayers()
+                .Where(p => p.PlayerName.Contains(identifier, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+
+            if (matchingPlayers.Count == 1)
+            {
+                return matchingPlayers.First();
+            }
+            if (matchingPlayers.Count > 1)
+            {
+                caller?.PrintToChat("Multiple players found with that name. Please refine your search.");
+            }
+            else
+            {
+                caller?.PrintToChat("No player found with that name.");
+            }
+
+            return null;
         }
 
         private static void Swap(CCSPlayerController player, CCSPlayerController target)
